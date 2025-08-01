e47d679ea91ff6f2fc44e4b799425d0ffe3ff57a
naveeenreddy12 <naveenreddyk2000@gmail.com>
2025-08-01 16:33:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const http = require("http");
 const socketIO = require("socket.io");
 const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
+
 
 const app = express();
 const server = http.createServer(app);
